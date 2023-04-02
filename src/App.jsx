0ff84253424a8f84a70ef996e1a99046a9cba5f6
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-04-02 21:41:05 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import Review from './Review';
 
 function App() {
   return (
-    <div className="App">
-<h1>hi</h1>
-    </div>
+    <main>
+    <section className="container">
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review/>
+    </section>
+    </main>
   );
 }
 
